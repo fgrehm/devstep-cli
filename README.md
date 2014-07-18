d668a7e68ba1e6099f87aae8e43ac87dd8b76595
Fabio Rehm <fgrehm@gmail.com>
2014-07-17 21:44:53 -0300

    Prototyping on `devstep hack`

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Work in progress rewrite of the [devstep](http://fgrehm.viewdocs.io/devstep) CLI
 
 ## TODO
 
-* [ ] `devstep hack`
+* [x] `devstep hack`
 * [ ] `devstep build`
+* [ ] `devstep hack` on top of a previously built image
 * [ ] `devstep bootstrap`
 * [ ] Custom `docker run` options for `devstep hack`
 * [ ] User and project specific configs for the `devstep` commands (current `.devsteprc`)
 * [ ] `devstep clean`
+* [ ] Figure out why we need to hit enter after exiting a docker attach
--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/fsouza/go-dockerclient"
+	"os"
 )
 
 func main() {
 	endpoint := "unix:///var/run/docker.sock"
 	client, _ := docker.NewClient(endpoint)
 
+	// Create container
 	container, err := client.CreateContainer(docker.CreateContainerOptions{
-		Name:  "container-from-api",
+		Name: "testing-new-devstep-cli",
 		Config: &docker.Config{
-			Image: "busybox",
+			//Image:        "ubuntu:14.04",
+			//Cmd:          []string{"/bin/bash"},
+			Image:        "fgrehm/devstep:v0.0.1",
+			Cmd:          []string{"/.devstep/bin/hack"},
+			OpenStdin:    true,
+			AttachStdin:  true,
+			AttachStdout: true,
+			AttachStderr: true,
+			Tty:          true,
 		},
 	})
 
@@ -22,21 +31,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(container)
+	// Start container
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	// imgs, err := client.ListImages(true)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
+	binds := []string{
+		pwd + ":/workspace",
+		"/tmp/devstep/cache:/.devstep/cache",
+	}
+	err = client.StartContainer(container.ID, &docker.HostConfig{
+		Binds: binds,
+	})
 
-	// for _, img := range imgs {
-	// 	fmt.Println("ID: ", img.ID)
-	// 	fmt.Println("RepoTags: ", img.RepoTags)
-	// 	fmt.Println("Created: ", img.Created)
-	// 	fmt.Println("Size: ", img.Size)
-	// 	fmt.Println("VirtualSize: ", img.VirtualSize)
-	// 	fmt.Println("ParentId: ", img.ParentId)
-	// 	fmt.Println("Repository: ", img.Repository)
-	// }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Attach to the container
+	err = client.AttachToContainer(docker.AttachToContainerOptions{
+		Container:    container.ID,
+		InputStream:  os.Stdin,
+		OutputStream: os.Stdout,
+		ErrorStream:  os.Stderr,
+		Stdin:        true,
+		Stdout:       true,
+		Stderr:       true,
+		Stream:       true,
+		RawTerminal:  true,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
