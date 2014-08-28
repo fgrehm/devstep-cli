2202ece8d93a38b11235e2712eae6088fa1465f3
Fabio Rehm <fgrehm@gmail.com>
2014-08-27 23:29:36 -0300

    Inline var

--- devstep/config_loader.go
+++ devstep/config_loader.go
@@ -41,8 +41,7 @@ func (l *configLoader) Load() (*ProjectConfig, error) {
 		return nil, err
 	}
 
-	homeConfigFile := l.homeDirectory + "/devstep.yml"
-	yamlConf, err := loadConfig(homeConfigFile)
+	yamlConf, err := loadConfig(l.homeDirectory + "/devstep.yml")
 	if err != nil {
 		return nil, err
 	}
